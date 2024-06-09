b625dbb3d0e392a943fe8194150a0dc2d468bc3d
MatiXdou <163367319+MatiXdou@users.noreply.github.com>
2024-06-09 19:47:08 -0400

    mejoras al sitio

--- principal/static/js/javascript.js
+++ principal/static/js/javascript.js
@@ -1,8 +1,44 @@
 $(document).ready(function() {
+    var nombre;
+    var apellido;
     var email;
+    var emailCorrecto;
+    var motivoContacto;
+    var whyContacto;
     var formato;
     var dns;
+    
+
+
+
+    /* Validaciones individuales */
+
+    $("#nombre").blur(function(){
+        nombre = $("#nombre").val().trim();
+        if (nombre.length >=3 && nombre.length <= 20) {
+            $("#error1").html("");
+            console.log('nombre validado correctamente.');
+        } else {
+            $("#error1").html("<p>El nombre debe tener entre 3 y 20 caracteres.</p>");
+            return;
+        }
+    });
+
+
+    $("#apellido").blur(function(){
+        apellido = $("#apellido").val().trim();
+        if (apellido.length >=3 && apellido.length <= 20) {
+            $("#error2").html("");
+            console.log('apellido validado correctamente.')
+        } else {
+            $("#error2").html("<p>El apellido debe tener entre 3 y 20 caracteres.</p>");
+            return;
+        }
+    });
+
+
     $("#email").blur(function(){
+        emailCorrecto=false;
         email = $("#email").val();
         if(email.length > 0){
             $.get("https://www.disify.com/api/email/" + email,
@@ -13,116 +49,155 @@ $(document).ready(function() {
                         if (dns==true) {
                             $("#error3").html("");
                             console.log('email validado correctamente.')
+                            emailCorrecto=true;
                         } else {
                             $("#error3").html('<p>El email no tiene un dominio valido.</p>');
                             $("#email").focus();
+                            emailCorrecto=false;
                             return;
                         }
                     } else {
                         $("#error3").html('<p>El email no tiene un formato valido.</p>');
-                            $("#email").focus();
-                            return;
-                    }
-                    
+                        $("#email").focus();
+                        emailCorrecto=false;
+                        return;
+                    }                    
                 });
         } else{
             $("#error3").html('<p>Debe rellenar este campo.</p>');
             $("#email").focus();
             return;
         }
-        
+        console.log("email: "+emailCorrecto)
 
     })
+    
 
-    $("#formulario-contacto").submit(function(event) {
+    $("#motivo-contacto").click(function(){
+        motivoContacto = $("#motivo-contacto").val();
+        if (motivoContacto!="defoption") {
+            $("#error4").html("");
+            console.log('Motivo validado correctamente.')
+        } else {
+            $("#error4").html("<p>Debe seleccionar un motivo.</p>");
+            return;
+        }
+    });
+
+
+    $("#why-contacto").blur(function(){
+        whyContacto = $("#why-contacto").val().trim();
+        if (whyContacto.length >=10 && whyContacto.length <= 200) {
+            $("#error5").html("");
+            console.log('Mensaje contacto validado correctamente.')
+        } else {
+            $("#error5").html("<p>El mensaje debe contener entre 10 y 200 caracteres.</p>");
+            return;
+        }
+    });
+
+
+    $("#edad").blur(function(){
+        edad = $("#edad").val();
+        if (edad >= 18) {
+            $("#error2").html("");
+            console.log('Edad validada correctamente.')
+        } else {
+            $("#error2").html("<p>La edad debe ser mayor o igual a 18.</p>");
+            return;
+        }
+    });
+
+
+    $("#genero").click(function(){
+        genero = $("#genero").val();
+        if (genero!="defoption") {
+            $("#error4").html("");
+            console.log('Genero validado correctamente.')
+        } else {
+            $("#error4").html("<p>Debe seleccionar un genero.</p>");
+            return;
+        }
+    });
+
+
+
+    /* Validaciones al enviar formulario */
+    $("#formulario-contacto").submit(function(event){
         /*declarar variables*/
-        var nombre = $("#nombre").val();
-        var apellido = $("#apellido").val();
+        nombre = $("#nombre").val().trim();
+        apellido = $("#apellido").val().trim();
         email = $("#email").val();
-        var motivoContacto = $("#motivo-contacto").val();
-        var whyContacto = $("#why-contacto").val();
+        motivoContacto = $("#motivo-contacto").val();
+        whyContacto = $("#why-contacto").val().trim();
 
         /*Validaciones*/
         if (nombre.length >=3 && nombre.length <= 20) {
-            event.preventDefault();
             $("#error1").html("");
             console.log('nombre validado correctamente.')
         } else {
-            event.preventDefault();
             $("#error1").html("<p>El nombre debe tener entre 3 y 20 caracteres.</p>");
             $("#nombre").focus();
+            event.preventDefault();
             return;
         }
 
 
         if (apellido.length >=3 && apellido.length <= 20) {
-            event.preventDefault();
             $("#error2").html("");
             console.log('apellido validado correctamente.')
         } else {
-            event.preventDefault();
             $("#error2").html("<p>El apellido debe tener entre 3 y 20 caracteres.</p>");
             $("#apellido").focus();
+            event.preventDefault();
             return;
         }
     
 
 
-        /*
-        if (dominio=="gmail.com") {
-            event.preventDefault();
-            $("#error3").html("");
+        
+        if(emailCorrecto == true) {
             console.log('email validado correctamente.')
         } else {
+            $("#error3").html('<p>Validando email...</p>');
             event.preventDefault();
-            $("#error3").html('<p>El email debe tener el dominio: "@gmail.com".</p>');
-            $("#email").focus();
-            dominio="";
             return;
         }
-        */
+        
 
 
 
         if (motivoContacto!="defoption") {
-            event.preventDefault();
             $("#error4").html("");
             console.log('Motivo validado correctamente.')
         } else {
-            event.preventDefault();
             $("#error4").html("<p>Debe seleccionar un motivo.</p>");
             $("#motivo-contacto").focus();
+            event.preventDefault();
             return;
         }
 
 
         if (whyContacto.length >=10 && whyContacto.length <= 200) {
-            event.preventDefault();
             $("#error5").html("");
             console.log('Mensaje contacto validado correctamente.')
         } else {
-            event.preventDefault();
             $("#error5").html("<p>El mensaje debe contener entre 10 y 200 caracteres.</p>");
             $("#why-contacto").focus();
+            event.preventDefault();
             return;
         }
 
-        
         alert("¡Se ha enviado tu mensaje correctamente!");
-        $("#nombre").val("");
-        $("#apellido").val("");
-        $("#email").val("");
-        $("#motivo-contacto").val("");
-        $("#why-contacto").val("");
-
-
-
     });
+
+
+
 
 
 $("#formulario-suscripcion").submit(function(event) {
     /*declarar variables*/
-    var nombre = $("#nombre").val();
+    var nombre = $("#nombre").val().trim();
     var email = $("#email").val();
     var edad = $("#edad").val();
     var genero = $("#genero").val();
@@ -130,54 +205,47 @@ $("#formulario-suscripcion").submit(function(event) {
 
     /*Validaciones*/
     if (nombre.length >=3 && nombre.length <= 20) {
-        event.preventDefault();
         $("#error1").html("");
         console.log('Nombre validado correctamente.')
     } else {
-        event.preventDefault();
         $("#error1").html("<p>El nombre debe tener entre 3 y 20 caracteres.</p>");
         $("#nombre").focus();
+        event.preventDefault();
+        return;
+    }
+
+
+    if(emailCorrecto == true) {
+        console.log('email validado correctamente.')
+    } else {
+        $("#error3").html('<p>Validando email...</p>');
+        event.preventDefault();
         return;
     }
 
 
     if (edad >= 18) {
-        event.preventDefault();
         $("#error2").html("");
         console.log('Edad validada correctamente.')
     } else {
-        event.preventDefault();
         $("#error2").html("<p>La edad debe ser mayor o igual a 18.</p>");
         $("#edad").focus();
+        event.preventDefault();
         return;
     }
 
 
     if (genero!="defoption") {
-        event.preventDefault();
         $("#error4").html("");
         console.log('Genero validado correctamente.')
     } else {
-        event.preventDefault();
         $("#error4").html("<p>Debe seleccionar un genero.</p>");
         $("#genero").focus();
+        event.preventDefault();
         return;
     }
 
-
-    
     alert("¡Usted se ha suscrito exitosamente.!");
-    $("#nombre").val("");
-    $("#email").val("");
-    $("#edad").val("");
-    $("#genero").val("");
-
-
-
 });
     });
-
-
-
-
     
